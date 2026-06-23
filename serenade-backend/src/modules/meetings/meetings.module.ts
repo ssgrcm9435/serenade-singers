ac19830ab6998789b7fd6555e3c0e7442eee4e4b
ssgrcm9435 <ssg.rcm9435@gmail.com>
2026-06-23 17:14:50 +0000

    Fix JwtService dependency in meetings module

--- serenade-backend/src/modules/meetings/meetings.module.ts
+++ serenade-backend/src/modules/meetings/meetings.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { PrismaModule } from '../../prisma/prisma.module';
 import { MeetingsController } from './meetings.controller';
 import { MeetingsService } from './meetings.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [MeetingsController],
   providers: [MeetingsService],
   exports: [MeetingsService],
