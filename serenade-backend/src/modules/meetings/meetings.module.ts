e81551268c5509efe42ccd11d37ab33a94430950
ssgrcm9435 <ssg.rcm9435@gmail.com>
2026-06-23 17:17:40 +0000

    Fix JwtService provider in meetings module

--- serenade-backend/src/modules/meetings/meetings.module.ts
+++ serenade-backend/src/modules/meetings/meetings.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from '../auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { MeetingsController } from './meetings.controller';
 import { MeetingsService } from './meetings.service';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'serenade-secret',
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
   controllers: [MeetingsController],
   providers: [MeetingsService],
   exports: [MeetingsService],
