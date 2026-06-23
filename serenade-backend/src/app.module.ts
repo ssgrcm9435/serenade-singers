6603fe0386272c831cfcee6a79c7a800a33a7c63
ssgrcm9435 <ssg.rcm9435@gmail.com>
2026-06-23 14:38:43 +0000

    Add JWT authentication module

--- serenade-backend/src/app.module.ts
+++ serenade-backend/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [PrismaModule, UsersModule, ],
+  imports: [PrismaModule, UsersModule, AuthModule, ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ serenade-backend/src/modules/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../../prisma/prisma.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
+
+  async login(email: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+
+    return {
+      access_token: this.jwt.sign({
+        sub: user.id,
+        email: user.email,
+        role: user.role,
+      }),
+      user,
+    };
+  }
+}
